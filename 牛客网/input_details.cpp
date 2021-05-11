04bfbb93c308bcb47dd20f90b6ecd7581c36008c
HuangCongQing <1756260160@qq.com>
2021-05-11 20:52:52 +0800

    input

---
+++ 牛客网/input_details.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Description: 各种输入总结
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-05-11 20:52:32
+ * @LastEditTime: 2021-05-11 20:52:40
+ * @FilePath: /LeetCode/牛客网/input_details.cpp
+ */
