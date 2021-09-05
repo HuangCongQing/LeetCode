65e716c10bcdcc31b8537996edafd829775d47fc
HuangCongQing <1756260160@qq.com>
2021-09-05 11:45:14 +0800

    add

---
+++ leetcode/42.接雨水.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=cpp
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = max(leftMax, height[left]);
+            rightMax = max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+};
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode-solution-tuvc/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// @lc code=end
+
