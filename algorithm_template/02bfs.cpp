bcfc553bf731a14ee81f840899213c52e284b8ab
HuangCongQing <1756260160@qq.com>
2021-09-12 14:45:57 +0800

    add: algorithm_template

---
+++ algorithm_template/02bfs.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Description: BFS（广度优秀搜索模板）
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-09-12 14:38:51
+ * @LastEditTime: 2021-09-12 14:38:52
+ * @FilePath: /LeetCode/algorithm_template/bfs.cpp
+ */
+
+//（C++模板）
+int BFS(TreeNode/string start, TreeNode/string  targrt ){
+
+
+}
+
---
+++ algorithm_template/KMP.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-09-12 14:43:44
+ * @LastEditTime: 2021-09-12 14:43:44
+ * @FilePath: /LeetCode/algorithm_template/KMP.cpp
+ */
