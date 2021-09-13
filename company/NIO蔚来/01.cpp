d4b60f525b2aee2f3fe77f2367f0ecf523d3fe9d
HuangCongQing <1756260160@qq.com>
2021-09-13 19:47:34 +0800

    update

---
+++ company/NIO蔚来/01.cpp
@@ -0,0 +1,28 @@
+// # // str1="acbcbcabbcca" # 4
+// # // str2="bcac" # 1 
+// 统计一个字符串在另一个字符串的次数
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  // cout << "Hello, World!" << endl;
+  string str="acbcbcabbcca";
+  string sub="bcac";
+  int count = 0;
+  int index = 0;
+  // while(index<str.length()){
+  //   if((index==str.indexOf(sub, index))!=-1){
+  //     count++;
+  //     index+=sub.length();
+  //   }
+  // }
+  for(int i=0;i<str.size();i++){
+    if
+  }
+  return count;
+  // return 0;
+}
