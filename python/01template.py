4e84c6f7d31aa39d43166bc1194b32d9d9593136
HuangCongQing <1756260160@qq.com>
2021-10-10 18:28:36 +0800

    add： 模板

---
+++ python/01template.py
@@ -0,0 +1,28 @@
+""" 
+1 list1.sort() # 升序
+2 list1.reverse() # 逆序
+3 len(list1)  # 长度
+list讲解：https://www.runoob.com/python/python-lists.html
+
+"""
+
+
+# 输入===========================
+
+## 数字
+# n, k, x = map(int, input().strip().split())
+# n, k, x = 8,2,3
+
+# vector = list(map(int, input().strip().split()))
+# vector = [1,1,5,8,12,13,20,22]
+
+# vector.sort()  # 升序
+# vector.reverse() # 降序
+# print(vector) # 
+
+## 字符串
+nlist = list(map(str, input().strip().split()))
+print(''.join(nlist))
+
+
+# 算法============================
